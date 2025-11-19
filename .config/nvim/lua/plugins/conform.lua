3ca40485f5191566430a7d3db4280125261abf11
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-11-19 08:14:52 -0300

    latest

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,12 +3,13 @@ return {
 	config = function()
 		require("conform").setup({
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 1000,
 				lsp_format = "fallback",
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				c = { "clangd" },
+				python = { "black" },
 			},
 		})
 	end,
