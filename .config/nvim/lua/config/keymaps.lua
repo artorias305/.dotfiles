7a6a32cc5fa0d436a73fa41683be4d997c5ffc55
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-12-23 21:07:03 -0300

    Some changes because yes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 local builtin = require("telescope.builtin")
 local map = vim.keymap.set
 
--- map("n", "<leader>f", builtin.find_files)
+map("n", "<leader>f", builtin.find_files)
 map("n", "<leader>g", builtin.live_grep)
 map("n", "<leader>t", builtin.colorscheme)
 map("n", "<leader>h", builtin.help_tags)
