c7ce05da5c4bcc228e199fd8da80953cea7f8d9c
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-03-08 20:12:43 -0300

    Is this safe

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set SPACEFISH_PROMPT_ADD_NEWLINE false
-
-starship init fish | source
-zoxide init fish | source
 fzf --fish | source
 
-alias cat="bat"
-alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias src="source ~/.config/fish/config.fish"
 alias lg="lazygit"
-alias cd="z"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
@@ -20,12 +13,6 @@ set --export PATH $BUN_INSTALL/bin $PATH
 
 fish_add_path ~/.opencode/bin
 fish_add_path ~/.cargo/bin
-
-export FZF_CTRL_T_OPTS="
-  --style full
-  --walker-skip .git,node_modules,target
-  --preview 'bat -n --color=always {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
 # fnm
 set FNM_PATH "/opt/homebrew/opt/fnm/bin"
