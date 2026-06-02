15af7ee1499f9d9a9279184a75ce626a580567e7
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-06-01 21:57:48 -0300

    trying out fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting ""
+
+alias cat="bat"
+alias grep="rg"
+alias ls="eza"
+alias find="fd"
+
+abbr -a tree "eza --tree"
+abbr -a ps procs
+abbr -a lg lazygit
+abbr -a src "source ~/.config/fish/config.fish"
+
+set -gx MANPAGER "nvim +Man!"
+
+zoxide init fish | source
