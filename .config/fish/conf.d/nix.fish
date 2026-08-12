edf63a7decb20c39f37e047a947f305d27d5e18f
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-08-12 12:10:59 -0300

    doom emacs

---
+++ .config/fish/conf.d/nix.fish
@@ -0,0 +1,2 @@
+
+if test -e /home/kiq/.nix-profile/etc/profile.d/nix.fish; . /home/kiq/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,20 +4,12 @@ end
 
 set -g fish_greeting ""
 
-alias ls="eza"
 alias cd="z"
-
-abbr -a tree "eza --tree"
-abbr -a ps procs
-abbr -a lg lazygit
-abbr -a src "source ~/.config/fish/config.fish"
-abbr -a cat "bat"
-abbr -a grep "rg"
-abbr -a find "fd"
-
-set -gx MANPAGER "nvim +Man!"
 
 zoxide init fish | source
 
 fish_add_path ~/.bun/bin
 fish_add_path ~/.opencode/bin
+fish_add_path ~/go/bin
+
+direnv hook fish | source
